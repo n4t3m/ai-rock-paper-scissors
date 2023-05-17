fab0ab9242e6fac61a22873fbff150c7c24bd03c
NateM135 <melwaninate@gmail.com>
2023-05-17 16:18:04 -0700

    added authentication with db

--- server/src/routes.py
+++ server/src/routes.py
@@ -1,8 +1,9 @@
 '''This module contains API calls for the RPS Game'''
 
-from flask import Blueprint, session, abort
+from flask import Blueprint, session, abort, request
 from src.util import hello_world
 from src.decorators import login_required
+from src.models import User
 
 rps_routes = Blueprint("rps_routes", __name__)
 
@@ -11,11 +12,32 @@ def hello_world_route():
     '''Simple Hello World API Call'''
     return hello_world()
 
-@rps_routes.route("/login", methods=["GET"])
+
+# @rps_routes.route("/login", methods=["GET"])
+# def login():
+#     '''Simple "Login" Route for testing, log into psuedo Guest User'''
+#     session['username']="Guest"
+#     return "You have been logged in!"
+
+@rps_routes.route("/login", methods=["POST"])
 def login():
-    '''Simple "Login" Route'''
-    session['username']="Guest"
-    return "You have been logged in!"
+    '''Login Route'''
+    username = request.form.get("username")
+    if not username:
+        # print("Username Not Provided!")
+        abort(400)
+    password = request.form.get("password")
+    if not password:
+        # print("Password not Provided!")
+        abort(400)
+
+    user_record = User.query.filter_by(username=username).first()
+
+    if user_record and user_record.verify_password(password):
+        session['username']=username
+        return "You have been logged in!"
+
+    return "Username/Password Combination Not Found"
 
 @rps_routes.route("/logout", methods=["GET"])
 def logout():
