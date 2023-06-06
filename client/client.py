02897bed35c93fca10f697312786c536db1fbaa7
NateM135 <melwaninate@gmail.com>
2023-06-05 19:54:50 -0700

    fixed up game class thing

--- client/client.py
+++ client/client.py
@@ -129,123 +129,120 @@ class Game :
 
         # ----- Background Image ----
         self.bgframe= Image.open('images/background.png')
-        photo = ImageTk.PhotoImage(self.bgframe)
-        self.bg_panel = Label(self.master, image=photo)
-        self.bg_panel.image = photo
+        self.photo = ImageTk.PhotoImage(self.bgframe)
+        self.bg_panel = Label(self.master, image=self.photo)
+        self.bg_panel.image = self.photo
         self.bg_panel.pack(fill = 'both', expand = 'yes')
 
         # ------ Labels and Buttons ------
 
-        frame = Frame(self.master, bg = '#ebe4d3', width = '580', height=180)
-        frame.place(x = 125, y = 60)
+        self.frame = Frame(self.master, bg = '#ebe4d3', width = '580', height=180)
+        self.frame.place(x = 125, y = 60)
 
         self.GLabel = Label(self.master, text="Rock Paper Scissor", font = ("yu gothic ui", 40, 'bold'), bg = '#ebe4d3', fg = 'black')
         self.GLabel.place(x = 125, y = 60) #300
 
-        player1_score_label = Label(self.master, text="Player 1: 0", font = ("yu gothic ui", 20), bg = '#ebe4d3', fg = 'black')
-        player1_score_label.place(x = 200, y = 150)
+        self.player1_score_label = Label(self.master, text="Player 1: 0", font = ("yu gothic ui", 20), bg = '#ebe4d3', fg = 'black')
+        self.player1_score_label.place(x = 200, y = 150)
 
-        vs_label = Label(self.master, text="VS", font = ("yu gothic ui", 20), bg = '#ebe4d3', fg ='black')
-        vs_label.place(x = 380, y = 200)
+        self.vs_label = Label(self.master, text="VS", font = ("yu gothic ui", 20), bg = '#ebe4d3', fg ='black')
+        self.vs_label.place(x = 380, y = 200)
 
-        player2_score_label = Label(self.master, text="Player 2: 0", font = ("yu gothic ui", 20), bg = '#ebe4d3',  fg = 'black')
-        player2_score_label.place(x = 450, y = 150)
+        self.player2_score_label = Label(self.master, text="Player 2: 0", font = ("yu gothic ui", 20), bg = '#ebe4d3',  fg = 'black')
+        self.player2_score_label.place(x = 450, y = 150)
 
-        result_label = Label(self.master, text="", font="normal 20 bold", bg="white", width=15, borderwidth=2, relief="solid")
-        result_label.pack(pady=20)
+        self.result_label = Label(self.master, text="", font="normal 20 bold", bg="white", width=15, borderwidth=2, relief="solid")
+        self.result_label.pack(pady=20)
 
         
-        b1 = Button(self.master, text="Rock", font= ("yu gothic ui", 15))
-        b2 = Button(self.master, text="Paper", font=("yu gothic ui", 15))
-        b3 = Button(self.master, text="Scissor", font=("yu gothic ui", 15))
+        self.b1 = Button(self.master, text="Rock", font= ("yu gothic ui", 15))
+        self.b2 = Button(self.master, text="Paper", font=("yu gothic ui", 15))
+        self.b3 = Button(self.master, text="Scissor", font=("yu gothic ui", 15))
 
-        b4 = Button(self.master, text="Stat", command = self.stat, font=("yu gothic ui", 15))
-        b4.place(x = 325, y = 200)
+        self.b4 = Button(self.master, text="Stat", command = self.stat, font=("yu gothic ui", 15))
+        self.b4.place(x = 325, y = 200)
 
-        b1.place(x = 290, y = 200)
-        b2.place(x = 370, y = 200)
-        b3.place(x = 455, y = 200)
+        self.b1.place(x = 290, y = 200)
+        self.b2.place(x = 370, y = 200)
+        self.b3.place(x = 455, y = 200)
 
         #Score variables
         self.player1_score = 0
         self.player2_score = 0
 
-        video = cv2.VideoCapture(-1)
+        self.video = cv2.VideoCapture(0)
 
-        if not video.isOpened():
+        if not self.video.isOpened():
             raise IOError("Camera not enabled")
 
         # Load the model. This should be located within the same directory and labeled the same as it was in the original notebook.
-        model = tf.keras.models.load_model("./rock_paper_scissors_mobilenet_v2.h5")
-        labels = ['rock', 'paper', 'scissors']
+        self.model = tf.keras.models.load_model("./rock_paper_scissors_mobilenet_v2.h5")
+        self.labels = ['rock', 'paper', 'scissors']
 
-        width = 128
-        height = 128
-
-        def capture_video():
-            cap = cv2.VideoCapture(0)
-
-            scheduler = BackgroundScheduler()
-            make_choice_job = scheduler.add_job(repository.make_choice, args=(session, ""), trigger="interval", seconds=10)
-            scheduler.start()
-            
-            def update_frame():
-                cap = cv2.VideoCapture(0)
-
-                ret, frame = cap.read()
-                if ret:
-                    frame = cv2.flip(frame, 1)  # Flip frame horizontally
-                    frame_tensor = tf.expand_dims(frame, 0)
-                    image = tf.cast(frame_tensor, tf.float32)
-                    image = image/255
-                    image = tf.image.resize(image, [width, height])
-                    result = model.predict(image)
-                    inferred_result = np.argmax(result)
-                    confidence = result[0][inferred_result]
-
-                    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    img = Image.fromarray(cv2image)
-                    imgtk = ImageTk.PhotoImage(image=img)
-                    video_label.imgtk = imgtk
-                    video_label.configure(image=imgtk)
-                    if confidence < 0.5:
-                        print("Rejected result - confidence too low.")
-                    else:
-                        b1.configure(font='sans 16', fg='black')
-                        b2.configure(font='sans 16', fg='black')
-                        b3.configure(font='sans 16', fg='black')
-                        
-                        if labels[inferred_result] == 'rock':
-                            b1.configure(font='sans 16 bold', fg='red')
-                        elif labels[inferred_result] == 'paper':
-                            b2.configure(font='sans 16 bold', fg='red')
-                        else:
-                            b3.configure(font='sans 16 bold', fg='red')
-                        # TODO: Switch to button from scheduler
-                        # make_choice_job.modify(args=(username, labels[inferred_result]))
-                        print(f"Inferred result: {labels[inferred_result]} with confidence {confidence*100}%")
-
-                video_label.after(10, update_frame)
-
-            update_frame()
-
-        # Update score function
-        def update_score(player):
-            print("Update")
-        
-        def stat() :
-            print("Do")
-
+        self.width = 128
+        self.height = 128
 
         # Add Labels, Frames, and Buttons
-
-
-        video_label = Label(self.master, width=400, height=400)
-        video_label.place(x = 220, y = 250)
+        self.video_label = Label(self.master, width=400, height=400)
+        self.video_label.place(x = 220, y = 250)
 
         # Start capturing video frames
-        capture_video()
-        self.master.mainloop()
+        self.cap = cv2.VideoCapture(0)
+        # self.capture_video()
+        self.update_frame()
+
+    # def capture_video(self):
+    #     cap = cv2.VideoCapture(0)
+
+    #     # scheduler = BackgroundScheduler()
+    #     # make_choice_job = scheduler.add_job(repository.make_choice, args=(session, ""), trigger="interval", seconds=10)
+    #     # scheduler.start()
+        
+    def update_frame(self):
+        print("here")
+        ret, frame = self.cap.read()
+        if ret:
+            print("inside")
+            frame = cv2.flip(frame, 1)  # Flip frame horizontally
+            frame_tensor = tf.expand_dims(frame, 0)
+            image = tf.cast(frame_tensor, tf.float32)
+            image = image/255
+            image = tf.image.resize(image, [self.width, self.height])
+            result = self.model.predict(image)
+            inferred_result = np.argmax(result)
+            confidence = result[0][inferred_result]
+
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv2image)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.video_label.imgtk = imgtk
+            self.video_label.configure(image=imgtk)
+            print("inside2")
+            if confidence < 0.5:
+                print("Rejected result - confidence too low.")
+            else:
+                self.b1.configure(font='sans 16', fg='black')
+                self.b2.configure(font='sans 16', fg='black')
+                self.b3.configure(font='sans 16', fg='black')
+                
+                if self.labels[inferred_result] == 'rock':
+                    self.b1.configure(font='sans 16 bold', fg='red')
+                elif self.labels[inferred_result] == 'paper':
+                    self.b2.configure(font='sans 16 bold', fg='red')
+                else:
+                    self.b3.configure(font='sans 16 bold', fg='red')
+                # TODO: Switch to button from scheduler
+                # make_choice_job.modify(args=(username, labels[inferred_result]))
+                print(f"Inferred result: {self.labels[inferred_result]} with confidence {confidence*100}%")
+            self.master.after(10, self.update_frame)
+
+
+    # Update score function
+    def update_score(self, player):
+        print("Update")
+    
+    def stat(self) :
+        print("Do")
 
 
 # --- MAIN WINDOW ---
