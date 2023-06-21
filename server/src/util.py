ed0bbe44933a0c75fcd168899d327a6d348dea9c
NateM135 <melwaninate@gmail.com>
2023-06-20 17:05:06 -0700

    formatting + better var names

--- server/src/util.py
+++ server/src/util.py
@@ -56,10 +56,11 @@ def getRecentMatchFromUsername(username):
     return most_recent_match
 
 def getRecentMatchData(username) -> dict:
+    '''Returns up to 10 most recent matches'''
     res = User.query.filter_by(username=username).first()
     if not res:
         return None
-    
+
     target_id = res.id
 
     most_recent_matches = MatchHistory.query.filter(
@@ -78,7 +79,9 @@ def getRecentMatchData(username) -> dict:
         match_object = {}
         if match.player_one_id == target_id:
             match_object["final_elo"] = match.player_one_final_elo
-            match_object["opponent_name"] = User.query.filter_by(id=match.player_two_id).first().username
+            match_object["opponent_name"] = User.query.filter_by(
+                id=match.player_two_id
+                ).first().username
             if match.winner == "1":
                 ret["wins"]+=1
                 match_object["result"] = "Win"
@@ -88,7 +91,9 @@ def getRecentMatchData(username) -> dict:
         else:
             # user is player 2
             match_object["final_elo"] = match.player_two_final_elo
-            match_object["opponent_name"] = User.query.filter_by(id=match.player_one_id).first().username
+            match_object["opponent_name"] = User.query.filter_by(
+                id=match.player_one_id
+                ).first().username
         if match.winner == "3":
             ret["ties"]+=1
             match_object["result"] = "Tie"
@@ -105,43 +110,48 @@ def getRecentMatchData(username) -> dict:
 
 def record_tie(p1username, p2username):
     '''no winner, no elo calculation'''
-    p1 = User.query.filter_by(username=p1username).first()
-    p2 = User.query.filter_by(username=p2username).first()
+    p_1 = User.query.filter_by(username=p1username).first()
+    p_2 = User.query.filter_by(username=p2username).first()
 
-    m = MatchHistory(
-        player_one_id=p1.id,
-        player_two_id=p2.id,
+    match_object = MatchHistory(
+        player_one_id=p_1.id,
+        player_two_id=p_2.id,
     )
 
-    m.player_one_final_elo, m.player_two_final_elo = m.player_one_initial_elo, m.player_two_initial_elo
-    m.player_one_ack = m.player_two_ack = True
-    m.winner = "3"
-    m.match_created = datetime.now()
+    match_object.player_one_final_elo = match_object.player_one_initial_elo
+    match_object.player_two_final_elo = match_object.player_two_initial_elo
+    match_object.player_one_ack = match_object.player_two_ack = True
+    match_object.winner = "3"
+    match_object.match_created = datetime.now()
 
-    db.session.add(m)
+    db.session.add(match_object)
     db.session.commit()
 
 def record_win(p1username, p2username, winner):
     '''winner==1 is p1win, winner==2 is p2win'''
-    p1 = User.query.filter_by(username=p1username).first()
-    p2 = User.query.filter_by(username=p2username).first()
+    p_1 = User.query.filter_by(username=p1username).first()
+    p_2 = User.query.filter_by(username=p2username).first()
 
-    m = MatchHistory(
-        player_one_id=p1.id,
-        player_two_id=p2.id,
+    match_object = MatchHistory(
+        player_one_id=p_1.id,
+        player_two_id=p_2.id,
     )
 
-    m.player_one_final_elo, m.player_two_final_elo = calculate_elo_change(m.player_one_initial_elo, m.player_two_initial_elo, str(winner))
-    User.query.filter_by(id=p1.id).first().elo = m.player_one_final_elo
-    User.query.filter_by(id=p2.id).first().elo = m.player_two_final_elo
-    m.player_one_ack = m.player_two_ack = True
-    m.winner = str(winner)
-    m.match_created = datetime.now()
+    match_object.player_one_final_elo, match_object.player_two_final_elo = calculate_elo_change(
+        match_object.player_one_initial_elo,
+        match_object.player_two_initial_elo,
+        str(winner)
+    )
+    User.query.filter_by(id=p_1.id).first().elo = match_object.player_one_final_elo
+    User.query.filter_by(id=p_2.id).first().elo = match_object.player_two_final_elo
+    match_object.player_one_ack = match_object.player_two_ack = True
+    match_object.winner = str(winner)
+    match_object.match_created = datetime.now()
 
-    db.session.add(m)
+    db.session.add(match_object)
     db.session.commit()
 
-def determine_rps_winner(p1_choice, p2_choice):
+def determine_rps_winner(p1_choice, p2_choice): #pylint: disable=R0911
     '''RPS. Return 1 if p1win. Return 2 is p2win. Return 0 if tie.'''
     if p1_choice == p2_choice:
         return 3
@@ -149,40 +159,41 @@ def determine_rps_winner(p1_choice, p2_choice):
     if p1_choice == "rock":
         if p2_choice == "paper":
             return 2
-        elif p2_choice == "scissors":
+        if p2_choice == "scissors":
             return 1
 
     if p1_choice == "paper":
         if p2_choice == "rock":
             return 1
-        elif p2_choice == "scissors":
+        if p2_choice == "scissors":
             return 2
 
     if p1_choice == "scissors":
         if p2_choice == "rock":
             return 2
-        elif p2_choice == "paper":
+        if p2_choice == "paper":
             return 1
 
     return None
 
 def playMatches(app):
+    '''Logic to play all queued matches'''
     with app.app_context():
         while app.config['QUEUE'].qsize() >= 2:
-            p1 = app.config['QUEUE'].get()
-            p2 = app.config['QUEUE'].get()
+            p_1 = app.config['QUEUE'].get()
+            p_2 = app.config['QUEUE'].get()
 
-            res = determine_rps_winner(p1.choice, p2.choice)
+            res = determine_rps_winner(p_1.choice, p_2.choice)
 
             if not res:
                 print("Could not determine RPS Winner")
-                return # TODO: log if invalid result is sent
+                return
             if res == 3:
                 # Tie, Record Match
-                record_tie(p1.username, p2.username)
+                record_tie(p_1.username, p_2.username)
             elif res==1:
                 # P1 Wins
-                record_win(p1.username, p2.username, "1")
+                record_win(p_1.username, p_2.username, "1")
             else:
                 # P2 Wins
-                record_win(p1.username, p2.username, "2")
+                record_win(p_1.username, p_2.username, "2")
